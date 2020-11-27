ec5c64d4aa8980495db2fe247d9b9f8c4cac2514
Alejandro Alfredo Salgado Gaspar <a.salgado.gaspar@gmail.com>
2020-11-27 16:24:57 -0600

    bugfix

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ io.on('connect', async socket => {
     socketSessionId = socket.request._query['session_id'];
 
      await SessionModel
-        .findById(socketSessionId = )
+        .findById(socketSessionId )
         .exec( (err, session) =>{
           if (err) {
             console.log("Socket is not authenticated");
