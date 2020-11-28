2e50f3b2712ab8ebee34d68d1c6612f64958be86
Alejandro Alfredo Salgado Gaspar <a.salgado.gaspar@gmail.com>
2020-11-27 18:07:21 -0600

    bugfix

--- app.js
+++ app.js
@@ -121,23 +121,21 @@ io.on('connect', async socket => {
               console.log("SOCKET IS AUTHENTICATED");
               socket.session_id = socketSessionId;
               socket.session_account = session.account;
-              socket["session_id"] = socketSessionId;
-              socket["session_account"] = session.account;
 
           }
 
         });
 
-        socket.on('sendMessage', async (message, callback) => {
+        socket.on('sendMessage', async function (message, callback) {
                 
-                if (!socket["session_account"]){
+                if (!this.session_account){
                     console.log("NO SENDER FOUND IN MESSAGE")
                     socket.disconnect();
                 } else{
 
                   try {
-                      console.log("MESSAGE RECEIVED FROM SESSION:", socket["session_id"]);
-                      var sender = socket["session_account"];
+                      console.log("MESSAGE RECEIVED FROM SESSION:", this.session_id);
+                      var sender = this.session_account;
                       createdMessage = await helpers.addOneMessage(message,sender);
 
                       if (createdMessage){
